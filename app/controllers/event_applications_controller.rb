c3b423810688df714edd9adb1eb0917c1abc2eff
Daniel Zhang <danielzhang@Daniels-MBP.fios-router.home>
2017-07-11 17:09:02 -0400

    added the needed code for email to be sent

--- app/controllers/event_applications_controller.rb
+++ app/controllers/event_applications_controller.rb
@@ -11,6 +11,14 @@ class EventApplicationsController < ApplicationController
     @application.application_status = @new_status
     @application.save
     redirect_to event_application_path(@application)
+    @user = User.find_by(id: @id)
+    if(@new_status == 'accepted')
+      UserMailer.accepted_email(@user).deliver_now
+    elsif(@new_status == 'denied')
+      UserMailer.denied_email(@user).deliver_now
+    else
+      UserMailer.waitlisted_email(@user).deliver_now
+    end
   end
 
 

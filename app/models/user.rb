4222a60060bc0420b690439262679f997c0ad956
Brian Ramirez <bramirez@umass.edu>
2017-07-10 21:18:02 -0700

    added validations for name and lastname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_presence_of :first_name, :last_name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 

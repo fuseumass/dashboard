f9d1043f8bcef9e1b1748796697a664f0091be78
Brian Ramirez <bramirez@umass.edu>
2017-07-11 23:35:00 -0700

    fixed logo on emails and allowed emailing on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hackumass-web_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'hackumass-web.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {

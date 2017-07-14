42a3893fcf207e986d7dc8c0cc343c3510316268
Brian Ramirez <bramirez@umass.edu>
2017-07-14 11:38:08 -0700

    code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :auth_user
-  autocomplete :university, :name, :full => true
-  autocomplete :major, :name, :full => true
 
   # Additional parameters needed for devise
   protected

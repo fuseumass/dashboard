c76e02e7692afe794e30f0874c5c8f4b97a9b313
Brian Ramirez <bramirez@umass.edu>
2017-07-17 14:33:17 -0700

    added deployment script and bumped version to 0.4.0

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+cat << "EOF"
+/$$   /$$                     /$$       /$$   /$$ /$$      /$$
+| $$  | $$                    | $$      | $$  | $$| $$$    /$$$
+| $$  | $$  /$$$$$$   /$$$$$$$| $$   /$$| $$  | $$| $$$$  /$$$$  /$$$$$$   /$$$$$$$  /$$$$$$$
+| $$$$$$$$ |____  $$ /$$_____/| $$  /$$/| $$  | $$| $$ $$/$$ $$ |____  $$ /$$_____/ /$$_____/
+| $$__  $$  /$$$$$$$| $$      | $$$$$$/ | $$  | $$| $$  $$$| $$  /$$$$$$$|  $$$$$$ |  $$$$$$
+| $$  | $$ /$$__  $$| $$      | $$_  $$ | $$  | $$| $$\  $ | $$ /$$__  $$ \____  $$ \____  $$
+| $$  | $$|  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$/| $$ \/  | $$|  $$$$$$$ /$$$$$$$/ /$$$$$$$/
+|__/  |__/ \_______/ \_______/|__/  \__/ \______/ |__/     |__/ \_______/|_______/ |_______/
+EOF
+
+echo ' '
+echo 'Preparing HackUMass Web App For Deployment.....'
+echo ' '
+echo 'Precompiling Assets...'
+RAILS_ENV=production bundle exec rake assets:precompile
+echo 'Assets precompiled succesfully ✅'
+echo ' '
+echo 'Pushing build to Heroku....'
+git push heroku master
+echo ' '
+echo 'Heroku Build Sucessfull ✅'
+echo 'Migrating databases....'
+heroku run rake db:migrate
+echo ' '
+echo 'Database Migration Succesfull ✅'

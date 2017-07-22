124c8168bd6fa62c3e290b4b14bca16dbf526a9b
Brian Ramirez <bramirez@umass.edu>
2017-07-22 12:20:38 -0700

    cleanning up navigation

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,50 @@
 module PagesHelper
+
+	def is_home_active
+		controller?("pages") and action?("index")
+	end
+
+	def is_hardware_active
+		controller?("hardware_items")
+	end
+
+	def is_admin_active
+		current_page?(admin_path)
+	end
+
+	def is_applications_active
+		controller?("event_applications")
+	end
+
+	def is_mentorship_active
+		controller?("mentoship_requests")
+	end
+
+	def has_access_to_applications
+		current_user.is_organizer? or current_user.is_admin?
+	end
+
+	def has_access_to_hardware
+		current_user.is_accepted? or current_user.is_admin?
+	end
+
+	def has_access_to_mentorship
+		current_user.is_admin? or current_user.is_mentor?
+	end
+
+	def has_access_to_admin
+		current_user.is_admin?
+	end
+
+
+	# Helper methonds to this helper class. lol so meta
+	def controller?(target_controller)
+		target_controller.include?(params[:controller])
+	end
+
+	def action?(target_action)
+		target_action.include?(params[:action])
+	end
+
+
 end

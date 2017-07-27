2e35edb7a573af3a7a2d05433a9e6fea35b6fc62
Brian Ramirez <bramirez@umass.edu>
2017-07-26 23:45:28 -0700

    forcing the app to be secure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hackumass-web_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'hackumass-web.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'dashboard.hackumass.com' }
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail

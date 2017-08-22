21d86e3f8011f8f2939678793f3c2dd976411639
Daniel Zhang <dbzhang@umass.edu>
2017-08-22 16:57:25 -0400

    add image uploading feature to event and hardware

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,7 +53,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :description, :location, :time, :created_by, :thumbnail)
+      params.require(:event).permit(:title, :description, :location, :time, :created_by, :thumbnail, :image)
     end
 
     #  Only admins and organizers have the ability to create, update, edit, show, and destroy Events

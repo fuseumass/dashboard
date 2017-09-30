f8220be92ca87997ec9fe0e4ec1df0e9b80fe1ad
Daniel Zhang <danielzhang@1x-nat-vl931-172-30-137-46.wireless.umass.edu>
2017-09-30 15:15:39 -0400

    added reminded email for all application who haven't complete their application

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,17 @@ Rails.application.configure do
   # Environment optons required by Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  #config.action_mailer.delivery_method = :smtp
+
+  #config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :user_name            => 'donotreply.hackumass@gmail.com',
+  #  :password             => 'redpanda112358',
+  #  :authentication       => "plain",
+  #  :enable_starttls_auto => true
+  #}
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 

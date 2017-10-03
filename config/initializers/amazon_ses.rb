87c066679ca1c2e3dfa781e5571e99b32b84be9f
Brian Ramirez <bramirez@umass.edu>
2017-10-03 11:44:33 -0400

    implement amazon ses

---
+++ config/initializers/amazon_ses.rb
@@ -0,0 +1,3 @@
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+                                       access_key_id: 'AKIAJP22YR65QF763O4Q',
+                                       secret_access_key: 'AlPL8vx0FGb4ua1aM/utyf1sRaiW4ImcdZKGYR2WSj1w'

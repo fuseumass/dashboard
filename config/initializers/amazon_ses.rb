83abd4bb283e5e37c9af4d7823da78cb49d2a0d6
Brian Ramirez <bramirez@umass.edu>
2017-10-03 11:48:56 -0400

    new user for amazon ses

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-                                       access_key_id: 'AKIAJP22YR65QF763O4Q',
-                                       secret_access_key: 'AlPL8vx0FGb4ua1aM/utyf1sRaiW4ImcdZKGYR2WSj1w'
+                                       access_key_id: 'AKIAI5ELW7IPJCGSWUQA',
+                                       secret_access_key: 'AnTH9NIGs7h6wJGae0wVSd6pO81KGpoChqb9Jr2uPFQx'

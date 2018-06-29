a66383f0725fca8c55bf127e94b66d8685b589b3
Brian Ramirez <brianramirez@Brians-MacBook-Pro-2.local>
2018-06-28 20:07:34 -0700

    ui changes started. nav bar done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require tabler
+//= require tabler.plugins
 //= require Chart.bundle
 //= require chartkick
 //= require highcharts
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require autocomplete-rails

260dcd701a762637030c8fa45582825c62ae0450
Brian Ramirez <brianramirez022798@gmail.com>
2018-08-18 09:09:56 -0700

    changed email templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,12 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user;
-    mail(to: @user.email, subject: 'Thank you for signing up for HackUMass V!')
+    mail(to: @user.email, subject: 'Thank you for signing up for HackUMass VI!')
   end
 
   def submit_email(user)
     @user = user;
-    mail(to: @user.email, subject: 'Thank you for submitting your HackUMass V application!')
+    mail(to: @user.email, subject: 'Thank you for submitting your HackUMass VI application!')
   end
 
   def accepted_email(user)
@@ -20,12 +20,12 @@ class UserMailer < ApplicationMailer
 
   def denied_email(user)
     @user = user;
-    mail(to: @user.email, subject: 'HackUMass V Application Status Update')
+    mail(to: @user.email, subject: 'HackUMass VI Application Status Update')
   end
 
   def waitlisted_email(user)
     @user = user;
-    mail(to: @user.email, subject: 'HackUMass V Application Status Update')
+    mail(to: @user.email, subject: 'HackUMass VI Application Status Update')
   end
 
   def reminder_email(user, message, subject)

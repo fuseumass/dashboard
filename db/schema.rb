80d6adadc50ddf77ec89f06deaa755236b60fc5e
Brian Ramirez <brianramirez022798@gmail.com>
2018-09-10 15:19:19 -0400

    adding polish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_015844) do
+ActiveRecord::Schema.define(version: 2018_09_09_211902) do
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
 
   create_table "emails", force: :cascade do |t|
     t.string "subject"
@@ -115,6 +130,10 @@ ActiveRecord::Schema.define(version: 2018_09_05_015844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "projectimage_file_name"
+    t.string "projectimage_content_type"
+    t.integer "projectimage_file_size"
+    t.datetime "projectimage_updated_at"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
@@ -140,6 +159,8 @@ ActiveRecord::Schema.define(version: 2018_09_05_015844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_type", default: "attendee"
+    t.boolean "rsvp", default: false
+    t.boolean "check_in", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

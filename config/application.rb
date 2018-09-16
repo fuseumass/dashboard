a793a472f6f0ac313ad6674ba0c344e29db4f415
Brian Ramirez <brianramirez022798@gmail.com>
2018-09-16 13:32:43 -0400

    minor additions

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module HackumassWeb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
 

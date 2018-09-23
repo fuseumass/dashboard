a8b9a578600e3499a68d59bee2b7b41168a1c5ec
Brian Ramirez <brianramirez022798@gmail.com>
2018-09-22 22:44:11 -0400

    added github stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -118,7 +118,7 @@ class PagesController < ApplicationController
   def remove_permissions
     @user = User.find(params[:format])
     if @user.email == 'bramirez@umass.edu'
-      redirect_to admin_path, alert: "Permission could not be removed. Brian's level of permission is God."
+      redirect_to admin_path, alert: "Permission could not be removed. Brian is God."
     else
       @user.user_type = 'attendee'
       @user.save

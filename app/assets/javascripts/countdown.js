6a2db978cf05f385678dcd78ed4750a93a3a4776
Brian Ramirez <brianramirez022798@gmail.com>
2018-09-25 14:05:28 -0400

    Added hacking countdown

---
+++ app/assets/javascripts/countdown.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 12, 2018 21:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = "Hacking Begins In: " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "0h 0m 0s... Let the hacking begin!";
+  }
+}, 1000);

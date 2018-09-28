a3a87c454f99f6cd77372141444275fe754905b1
Ryan Mahan <ryan@Ryans-MBP.hsd1.ma.comcast.net>
2018-09-27 23:48:01 -0400

    Added tags

--- app/models/mentorship_request.rb
+++ app/models/mentorship_request.rb
@@ -1,5 +1,5 @@
 class MentorshipRequest < ApplicationRecord
-	validates_presence_of :user, :title, :status, :urgency, :help_type
-	validates :help_type, format: {with: /\S/, message: "Please choose one."}
+	validates_presence_of :user, :title, :status, :urgency
+	# validates :help_type, format: {with: /\S/, message: "Please choose one."}
 	belongs_to :user
 end

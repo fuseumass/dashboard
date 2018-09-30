c7aeeae3b526df75b3018dded3d9ee82efc853ee
Ryan Mahan <ryan@Ryans-MBP.hsd1.ma.comcast.net>
2018-09-29 22:17:44 -0400

    added image support for MRequests

--- app/models/mentorship_request.rb
+++ app/models/mentorship_request.rb
@@ -1,5 +1,7 @@
 class MentorshipRequest < ApplicationRecord
 	validates_presence_of :user, :title, :status, :urgency
+	has_attached_file :screenshot
+	validates_attachment_content_type :screenshot, :content_type => /image/
 	serialize :tech, Array
 	belongs_to :user
 end

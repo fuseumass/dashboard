94a45720504bee909101979cb963b4c5eb1de7d2
Gaurab Pant <bpant@umass.edu>
2018-10-05 23:34:08 -0400

    Added pagination, also tried to make the button better, still feel ehh.

--- app/controllers/mentorship_requests_controller.rb
+++ app/controllers/mentorship_requests_controller.rb
@@ -7,7 +7,7 @@ class MentorshipRequestsController < ApplicationController
 
     @search = MentorshipRequest.ransack(params[:q])
 
-    @mentorship_requests = @search.result
+    @mentorship_requests = @search.result.paginate(page: params[:page], per_page: 10)
     ##@search.build_condition
   end
 
--- app/models/mentorship_request.rb
+++ app/models/mentorship_request.rb
@@ -4,4 +4,8 @@ class MentorshipRequest < ApplicationRecord
 	validates_attachment_content_type :screenshot, :content_type => /image/
 	serialize :tech, Array
 	belongs_to :user
+	
+	ransacker :name_case_insensitive, type: :string do
+		arel_table[:name].lower
+	  end
 end

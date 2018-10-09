2da49b1b978b8d08bd1541d435ea794dd6557f7e
Brian Ramirez <brianramirez022798@gmail.com>
2018-10-09 18:26:05 -0400

    removed acces to hardware and prizes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :auth_user
   before_action :set_raven_context
-  after_filter  :set_access_control_headers
+  after_action  :set_access_control_headers
   autocomplete :university, :name, full: true
   autocomplete :major, :name, full: true
 

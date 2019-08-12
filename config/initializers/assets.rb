f9a8f1ce5078f36b3089cee18cb54d617eceaf46
James Woglom <j@wogloms.net>
2019-08-12 01:15:40 -0400

    move logos to submodule

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,10 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.paths << Rails.root.join("hackathon-config", "assets", "images")
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( event_application.js )
+Rails.application.config.assets.precompile += ['*.png']

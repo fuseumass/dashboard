9f4e2e6b8fcaf694511209f8709bf3c057890b68
James Woglom <j@wogloms.net>
2019-08-18 17:23:49 -0400

    fix javascript countdown bug

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -16,13 +16,17 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = "Hacking Begins In: " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  if (document.getElementById("countdown") != null) {
+    // Display the result in the element with id="demo"
+    document.getElementById("countdown").innerHTML = "Hacking Begins In: " + hours + "h "
+    + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "0h 0m 0s... Let the hacking begin!";
+    }
+  } else {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "0h 0m 0s... Let the hacking begin!";
   }
 }, 1000);

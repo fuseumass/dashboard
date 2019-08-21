d28014dfb49088eb46c1261249ff51615c3c592e
James Woglom <j@wogloms.net>
2019-08-21 15:48:30 -0400

    update deploy script

--- docker_shell.sh
+++ docker_shell.sh
@@ -1,1 +1,8 @@
-docker exec -it `docker ps --format '{{.ID}}' -f 'ancestor=hackathon-dashboard'` ${@:-/bin/bash}
+RUNNING_ID=`docker ps --format '{{.ID}}' -f 'ancestor=hackathon-dashboard'`
+if [[ "$RUNNING_ID" == "" ]]; then
+    echo Running new container instance
+    ./docker_run.sh ${@:-/bin/bash}
+else
+    echo Connecting to running container instance $RUNNING_ID
+    docker exec -it $RUNNING_ID ${@:-/bin/bash}
+fi

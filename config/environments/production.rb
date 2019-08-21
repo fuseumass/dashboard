1345d708695ffccaa8a1c9f639e773558e4e3a64
James Woglom <j@wogloms.net>
2019-08-21 16:21:15 -0400

    set perform_deliveries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
       :enable_starttls_auto => true
   }
 
+  config.action_mailer.perform_deliveries = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false

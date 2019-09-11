f9ecf9c7ee52677cdd500fe51ba0edbcea0814d4
Senarii <Vortekdynamic@gmail.com>
2019-09-11 16:47:52 -0400

    Prevent admin/organizer from being team members

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -113,6 +113,8 @@ class ProjectsController < ApplicationController
         @user = User.where(email: params[:add_team_member]).first
         if @user.nil?
           redirect_to project_team_path(@project), alert: "Unable to add team member. Ensure the email is spelled correctly."
+        elsif @user.is_admin? or @user.is_mentor? or @user.is_organizer?
+          redirect_to project_team_path(@project), alert: "Unable to add administrators, organizers, or mentors as a team member."
         else
           @project.user << @user
           redirect_to project_team_path(@project), notice: "#{params[:add_team_member]} successfully added to team."

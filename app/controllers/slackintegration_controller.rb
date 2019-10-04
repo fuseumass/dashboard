06d447d44e918dac54a1aef5b378f26b10323534
James Woglom <j@wogloms.net>
2019-10-03 21:46:11 -0400

    get slack user email

--- app/controllers/slackintegration_controller.rb
+++ app/controllers/slackintegration_controller.rb
@@ -16,11 +16,14 @@ class SlackintegrationController < ActionController::Base
       render json: { "challenge" => params[:challenge] }
     elsif params[:type] == 'event_callback'
       if params[:event][:type] == 'team_join'
-        email = params[:event][:user][:profile][:email]
+        user_id = params[:event][:user][:id]
+        email = get_user_email(user_id)
         user = User.where(:email => email)
-        if user.exists?
+        if email == nil or email.length == 0
+          render json: { "error" => "no email for id" }
+        elsif user.exists?
           puts "Team join user exists"
-          user[0].slack_id = params[:event][:user][:id]
+          user[0].slack_id = user_id
           user[0].save
           notify_user(params[:event][:user][:id], "Hi there #{user[0].first_name} #{user[0].last_name}. Congrats, you've successfully joined the #{hackathonname} Slack! Head back to #{HackumassWeb::Application::DASHBOARD_URL} for more event logistics, or join some relevant channels here!")
           render json: { "ok" => "saved_user" }
@@ -66,5 +69,32 @@ class SlackintegrationController < ActionController::Base
     end
     puts response.body
 
+  end
+
+  def get_user_email(user_id)
+    bot_accesstok = HackumassWeb::Application::SLACKINTEGRATION_BOT_ACCESS_TOKEN
+    if not bot_accesstok or bot_accesstok.length == 0
+      puts "No bot access token"
+      return false
+    end
+    puts "Getting email of #{user_id}"
+    
+    uri = URI.parse("https://slack.com/api/users.info?token=#{bot_accesstok}&user=#{user_id}")
+    request = Net::HTTP::Get.new(uri)
+    puts request
+    
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    res = JSON.parse(response.body)
+    puts res
+    if res["ok"]
+      return res["user"]["profile"]["email"]
+    end
   end
 end

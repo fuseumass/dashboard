6ca7fe0ec6f7af12a40ac45e1fa0a7d5adc14d76
James Woglom <j@wogloms.net>
2019-10-03 21:29:19 -0400

    event param

--- app/controllers/slackintegration_controller.rb
+++ app/controllers/slackintegration_controller.rb
@@ -14,18 +14,22 @@ class SlackintegrationController < ActionController::Base
       render json: { "error" => "invalid_token" }
     elsif params[:type] == 'url_verification'
       render json: { "challenge" => params[:challenge] }
-    elsif params[:type] == 'team_join'
-      email = params[:user][:profile][:email]
-      user = User.where(:email => email)
-      if user.exists?
-        puts "Team join user exists"
-        user[0].slack_id = params[:user][:id]
-        user[0].save
-        notify_user(params[:user][:id], "Hi there #{user[0].first_name} #{user[0].last_name}. Congrats, you've successfully joined the #{hackathonname} Slack! Head back to #{HackumassWeb::Application::DASHBOARD_URL} for more event logistics, or join some relevant channels here!")
-        render json: { "ok" => "saved_user" }
+    elsif params[:type] == 'event_callback'
+      if params[:event][:type] == 'team_join'
+        email = params[:event][:user][:profile][:email]
+        user = User.where(:email => email)
+        if user.exists?
+          puts "Team join user exists"
+          user[0].slack_id = params[:event][:user][:id]
+          user[0].save
+          notify_user(params[:event][:user][:id], "Hi there #{user[0].first_name} #{user[0].last_name}. Congrats, you've successfully joined the #{hackathonname} Slack! Head back to #{HackumassWeb::Application::DASHBOARD_URL} for more event logistics, or join some relevant channels here!")
+          render json: { "ok" => "saved_user" }
+        else
+          notify_user(params[:event][:user][:id], "Hey there, we weren't able to link your Slack account with the #{hackathonname} Dashboard -- #{email} isn't registered for any accounts in our system. Make sure that you entered the same email address that you used to apply to the event while creating your Slack account.")
+          render json: { "ok" => "no_user" }
+        end
       else
-        notify_user(params[:user][:id], "Hey there, we weren't able to link your Slack account with the #{hackathonname} Dashboard -- #{email} isn't registered for any accounts in our system. Make sure that you entered the same email address that you used to apply to the event while creating your Slack account.")
-        render json: { "ok" => "no_user" }
+        render json: { "error" => "unknown" }
       end
     else
       render json: { "error" => "unknown" }

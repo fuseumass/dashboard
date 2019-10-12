a034f05cf2fd279def3b8048d63c67c4f3a9f408
evan <evan.fellman@gmail.com>
2019-10-12 18:50:56 -0400

    ooo pretty NFDL

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,18 +4,4 @@ class Event < ApplicationRecord
   validates_presence_of :start_time, :location, :title, :description
   has_many :event_attendances
   has_many :users, through: :event_attendances
-
-  def numberRSVPd
-    counter = 0
-    EventAttendance.where(event_id: self.id).each do |ea|
-      begin
-        user = User.find_by(id: ea.user_id) 
-          if user.is_attendee?
-            counter += 1 
-          end 
-      rescue => exception
-      end
-    end 
-    return counter      
-  end
 end

9d707f384878b3230b8ef1ceeaf7405fc5a9edff
James Woglom <j@wogloms.net>
2019-10-13 17:46:47 -0400

    fix

--- db/migrate/20191013203003_update_prizes_in_projects.rb
+++ db/migrate/20191013203003_update_prizes_in_projects.rb
@@ -1,10 +1,6 @@
 class UpdatePrizesInProjects < ActiveRecord::Migration[5.2]
   def change
     remove_column :projects, :prizes
-<<<<<<< HEAD
-    add_column :projects, :prizes, :json, default: []
-=======
     add_column :projects, :prizes, :json, array: true, default: []
->>>>>>> modify migration for prod environment -- WILL DELETE PRIZE SELECTIONS FOR PROJECT
   end
 end

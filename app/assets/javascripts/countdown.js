535b2eaa8a3c1e157f65755d2225fba952861721
James Woglom <j@wogloms.net>
2019-10-15 22:25:29 -0400

    hacking begins countdown

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -1,5 +1,6 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 12, 2018 24:00:00").getTime();
+// e.g. "Oct 12, 2018 24:00:00"
+var countDownDate = new Date(document.getElementById("countdown").getAttribute("data-date")).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {

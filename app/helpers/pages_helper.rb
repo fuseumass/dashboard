63156f3d631d0f1b882e089f55432301ed932c3a
Senarii <Vortekdynamic@gmail.com>
2019-10-17 14:34:05 -0400

    Initial Feature Flag Creation

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -36,6 +36,10 @@ module PagesHelper
       controller?("projects")
   end
 
+  def is_project_submissions_active?
+      controller?("project_submissions")
+  end
+
   def is_prizes_active?
       controller?("prizes")
   end

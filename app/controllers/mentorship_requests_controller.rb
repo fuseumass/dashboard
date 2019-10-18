18c046cece7600dc709fd6cd82d5f7aff6d8cc6f
James Woglom <j@wogloms.net>
2019-10-18 13:26:37 -0400

    hacky fix

--- app/controllers/mentorship_requests_controller.rb
+++ app/controllers/mentorship_requests_controller.rb
@@ -175,9 +175,9 @@ class MentorshipRequestsController < ApplicationController
 
     def mentor_notifications_with_tech(tech)
       if Rails.env.production?
-        MentorshipNotification.where("tech::varchar LIKE ? OR tech::varchar LIKE ?", "%#{tech}'%", "%#{tech}\"%")
+        MentorshipNotification.where("tech::varchar LIKE ?", "%#{tech}%")
       else
-        MentorshipNotification.where("tech LIKE ? OR tech LIKE ?", "%#{tech}'%", "%#{tech}\"%")
+        MentorshipNotification.where("tech LIKE ?", "%#{tech}%")
       end
     end
 

d4a80c7ad12b6f6b377c112a42f81233de28cc82
AVS1508 <aditya.11.present@gmail.com>
2022-11-04 12:10:33 -0400

    Rake migration for hardware items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_150340) do
+ActiveRecord::Schema.define(version: 2022_11_02_014907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,7 +111,7 @@ ActiveRecord::Schema.define(version: 2022_10_13_150340) do
   end
 
   create_table "hardware_items", id: :serial, force: :cascade do |t|
-    t.integer "upc"
+    t.integer "uid"
     t.string "name"
     t.string "link"
     t.string "category"
